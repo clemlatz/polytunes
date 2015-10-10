a8b27e31169947ca56ac06f81ac749232091740a
nbeydoon <zero000@hotmail.fr>
2015-10-10 23:47:43 +0200

    class

--- client/client.js
+++ client/client.js
@@ -66,30 +66,28 @@ Template.controls.events({
   }
 });
 
+class Instrument {
+  getWad() {
+    return new Wad({source : 'sine'});
+  }
+
+  playNote(frequency) {
+    var wad = this.getWad()
+    wad.play({
+      pitch : frequency,  // A4 is 440 hertz.
+      env : {
+        decay: duration / 1000 * .1,
+        hold: duration / 1000 * 1.1,
+        release: duration / 1000 * .75
+      },
+      reverb: {
+        wet: 1
+      }
+    });
+  }
+}
+
 Meteor.startup( function() {
-
-  var Instrument = function() {
-  }
-
-  Instrument.prototype = {
-    getWad: function() {
-      return new Wad({source : 'sine'});
-    },
-    playNote: function(frequency) {
-      var wad = this.getWad()
-      wad.play({
-        pitch : frequency,  // A4 is 440 hertz.
-        env : {
-          decay: duration / 1000 * .1,
-          hold: duration / 1000 * 1.1,
-          release: duration / 1000 * .75
-        },
-        reverb: {
-          wet: 1
-        }
-      });
-    }
-  }
 
   instrument = new Instrument();
 });
