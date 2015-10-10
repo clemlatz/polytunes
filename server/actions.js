8eabfb5d6e93b0b8471c53df3b3b8e2f052c1198
nbeydoon <zero000@hotmail.fr>
2015-10-10 19:54:24 +0200

    add note, delete note, add player, delete player

---
+++ server/actions.js
@@ -0,0 +1,45 @@
+Meteor.methods({
+	addNote: (roomId, {x, y, timestamp})=> {
+		var userId = Meteor.userId();
+		Rooms.update(roomId, {
+			$push: {
+				partition: {
+					x,
+					y,
+					timestamp,
+					userId
+				}
+			}
+		});
+	},
+	deleteNote: (roomId, {x, y})=> {
+		Rooms.update(roomId, {
+				$pull: {
+					partition: {
+						x,
+						y
+					}
+				}
+			});
+	},
+	addUser: (roomId, {surname, color})=> {
+		var userId = Meteor.userId();
+
+		Rooms.update(roomId, {
+			$push: {
+				players: {
+					surname,
+					userId,
+					color
+				}
+			}
+		});
+	},
+	deleteUser: (roomId, userId)=> {
+		Rooms.remove(roomId, {
+			$pull: {
+				userId
+			}
+		});
+	}
+});
