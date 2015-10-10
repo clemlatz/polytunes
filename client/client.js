5aaf1aaba898fafcdf83c87c6d48566e751dcd75
nbeydoon <zero000@hotmail.fr>
2015-10-10 23:49:52 +0200

    change location for class instatrument

--- client/client.js
+++ client/client.js
@@ -66,27 +66,6 @@ Template.controls.events({
   }
 });
 
-class Instrument {
-  getWad() {
-    return new Wad({source : 'sine'});
-  }
-
-  playNote(frequency) {
-    var wad = this.getWad()
-    wad.play({
-      pitch : frequency,  // A4 is 440 hertz.
-      env : {
-        decay: duration / 1000 * .1,
-        hold: duration / 1000 * 1.1,
-        release: duration / 1000 * .75
-      },
-      reverb: {
-        wet: 1
-      }
-    });
-  }
-}
-
 Meteor.startup( function() {
 
   instrument = new Instrument();
