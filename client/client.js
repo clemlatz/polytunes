da85ca2eb0a3ec8f49543781d1b6abda0c767a12
nbeydoon <zero000@hotmail.fr>
2015-10-10 16:46:23 +0200

    ok

--- client/client.js
+++ client/client.js
@@ -7,7 +7,7 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
-  'click button': function () {
+  'click button': ()=> {
     // increment the counter when button is clicked
     Session.set('counter', Session.get('counter') + 1);
   }
