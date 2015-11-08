2d91c97eb46ce46590c2b5c56af93e2316c38fb7
Clement Bourgoin <cb@nokto.net>
2015-11-08 02:18:20 +0100

    Add iOS icons

--- mobile-config.js
+++ mobile-config.js
@@ -1,1 +1,18 @@
 App.accessRule('*');
+
+App.info({
+  name: 'Polytunes',
+  description: 'An online collaborative music game',
+  version: '0.7.1',
+  author: 'Clément Bourgoin',
+  email: 'cb@nokto.net',
+  website: 'http://polytun.es'
+});
+
+App.icons({
+  'iphone': 'public/icons/apple-touch-icon-60x60.png',
+  'iphone_2x': 'public/icons/apple-touch-icon-120x120.png',
+  'iphone_3x': 'public/icons/apple-touch-icon-152x152.png',
+  'ipad': 'public/icons/apple-touch-icon-76x76.png',
+  'ipad_2x': 'public/icons/apple-touch-icon-152x152.png'
+});
